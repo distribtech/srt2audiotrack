c8deaa46f0f34e1768206b3a8174a0b53a47d34b
mrbloom <mrbloom@ukr.net>
2025-07-31 14:17:35 +0300

    Move to simple python envoronment

---
+++ tts-recursive_oterra_standart-venv.bat
@@ -0,0 +1,16 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM === Set the path to the folder containing the .srt files ===
+set "SUBTITLE_FOLDER=\\10.98.100.14\distr\fast_channels\OTERRA\AUDIO_ENG\STANDART_VOICE"
+
+CALL "venv\Scripts\activate.bat"
+
+REM === Main loop ===
+:loop
+python main.py --subtitle "%SUBTITLE_FOLDER%" --output_folder "%SUBTITLE_FOLDER%"
+IF ERRORLEVEL 1 (
+    echo [ERROR] Script crashed. Waiting before retry...
+    timeout /t 5
+)
+goto loop
