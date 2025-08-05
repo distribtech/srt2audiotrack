e435bd914ac108d6f59afc75948e98350c4574ee
mrbloom <mrbloom@ukr.net>
2025-08-05 13:19:43 +0300

    readme.md block-scheme is wrong

--- sub-edit/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders subtitle editor heading', () => {
-  render(<App />);
-  const heading = screen.getByText(/subtitle editor/i);
-  expect(heading).toBeInTheDocument();
-});
