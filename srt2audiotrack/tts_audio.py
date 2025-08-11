7763dc7cdca0b6504bc52b2e8f0ead1f437a4ab3
mrbloom <mrbloom@ukr.net>
2025-08-11 20:53:31 +0300

    too low level

--- srt2audiotrack/tts_audio.py
+++ srt2audiotrack/tts_audio.py
@@ -438,7 +438,7 @@ class F5TTS:
         print(f"gen text: {gen_text}")
         print(f"Reference text: {ref_text}")
         print(f"Reference file: {ref_file}")
-        input("Press Enter to continue...")
+        # input("Press Enter to continue...")
         
         speeds = [0.3,0.4,0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5]
         rows = []
